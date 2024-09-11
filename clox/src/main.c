8ed02c9d1700855d8d437002826a1623e444abee
tommi <tommi.felle@gmail.com>
2024-09-11 12:09:14 +0200

    Until chapter 15 of the books. The interpreter can interpret the operations and do some arithmetic

--- clox/src/main.c
+++ clox/src/main.c
@@ -1,10 +1,38 @@
 #include <common.h>
 #include <chunk.h>
+#include <debug.h>
+#include <vm.h>
+#include <stdio.h>
 
 int main(int argc, const char* argv[]) {
-	Chunk chunk;
-	initChunk(&chunk);
-	writeChunk(&chunk, OP_RETURN);
-	freeChunk(&chunk);
-	return 0;
+    initVM();
+
+    Chunk chunk;
+    initChunk(&chunk);
+
+    int constantIdx = addConstant(&chunk, 1.2);
+    writeChunk(&chunk, OP_CONSTANT, 123);
+    writeChunk(&chunk, constantIdx, 123);
+
+    constantIdx = addConstant(&chunk, 3.4);
+    writeChunk(&chunk, OP_CONSTANT, 123);
+    writeChunk(&chunk, constantIdx, 123);
+
+    writeChunk(&chunk, OP_ADD, 123);
+
+    constantIdx = addConstant(&chunk, 5.6);
+    writeChunk(&chunk, OP_CONSTANT, 123);
+    writeChunk(&chunk, constantIdx, 123);
+
+    writeChunk(&chunk, OP_DIVIDE, 123);
+
+    writeChunk(&chunk, OP_NEGATE, 123);
+    writeChunk(&chunk, OP_RETURN, 123);
+
+    printf("\t== INTERPRETER ==");
+    interpret(&chunk);
+    freeChunk(&chunk);
+
+    freeVM();
+    return 0;
 }
